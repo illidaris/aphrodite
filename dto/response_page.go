fea6369f97483a641eb4ecb2c19e485e3f298c1d
joseph <wangjianhua@ztgame.com>
2023-11-15 17:45:05 +0800

    add

--- dto/response_page.go
+++ dto/response_page.go
@@ -1,35 +1,22 @@
 package dto
 
+type IRow interface {
+	ToRow() []string
+}
+
 type Pager struct {
 	Page
 	TotalRecord int64 `json:"total"`
 	TotalPage   int64 `json:"totalPage"`
 }
 
-type DataPager struct {
-	Data interface{} `json:"data"`
-	Pager
+func (r Pager) GetTotal() int64 {
+	return r.TotalRecord
 }
-
-func NewDataPager(data interface{}, index, size int64, total int64) *DataPager {
-	res := &DataPager{}
-	res.PageIndex = index
-	res.PageSize = size
-	res.TotalRecord = total
-	res.Data = data
-	res.Paginator()
-	return res
-}
-
 func (p *Pager) Paginator() *Pager {
 	p.TotalPage = getTotalPage(p.TotalRecord, p.GetSize())
 	return p
 }
-
-func (p *DataPager) GetTotalRecord() int64 {
-	return p.TotalRecord
-}
-
 func getTotalPage(total int64, pageSize int64) int64 {
 	if pageSize == 0 {
 		return 1
@@ -38,4 +25,56 @@ func getTotalPage(total int64, pageSize int64) int64 {
 		return total / pageSize
 	}
 	return total/pageSize + 1
+}
+
+type RowPager RecordPager[IRow]
+
+func (r RowPager) ToRows() [][]string {
+	rows := [][]string{}
+	for _, record := range r.Data {
+		rows = append(rows, record.ToRow())
+	}
+	return rows
+}
+
+func NewRowPager(index, size int64, total int64, records ...IRow) *RowPager {
+	res := new(RowPager)
+	res.PageIndex = index
+	res.PageSize = size
+	res.TotalRecord = total
+	res.Data = records
+	res.Paginator()
+	return res
+}
+
+type RecordPager[T any] struct {
+	Data []T `json:"data"`
+	Pager
+}
+
+func NewRecordPager[T any](index, size int64, total int64, records ...T) *RecordPager[T] {
+	res := new(RecordPager[T])
+	res.PageIndex = index
+	res.PageSize = size
+	res.TotalRecord = total
+	res.Data = records
+	res.Paginator()
+	return res
+}
+
+// Deprecate : 后期启用
+type DataPager struct {
+	Data interface{} `json:"data"`
+	Pager
+}
+
+// Deprecate : 后期启用
+func NewDataPager(data interface{}, index, size int64, total int64) *DataPager {
+	res := &DataPager{}
+	res.PageIndex = index
+	res.PageSize = size
+	res.TotalRecord = total
+	res.Data = data
+	res.Paginator()
+	return res
 }
--- dto/response_page_test.go
+++ dto/response_page_test.go
@@ -11,7 +11,27 @@ func TestNewDataPager(t *testing.T) {
 		convey.Convey("NewDataPager", func() {
 			pager := NewDataPager(nil, 2, 10, 20)
 			convey.So(pager.TotalPage, convey.ShouldEqual, 2)
-			convey.So(pager.GetTotalRecord(), convey.ShouldEqual, 20)
+			convey.So(pager.GetTotal(), convey.ShouldEqual, 20)
+		})
+	})
+}
+
+func TestNewRowPager(t *testing.T) {
+	convey.Convey("TestNewRowPager", t, func() {
+		convey.Convey("NewRowPager", func() {
+			pager := NewRowPager(2, 10, 20)
+			convey.So(pager.TotalPage, convey.ShouldEqual, 2)
+			convey.So(pager.GetTotal(), convey.ShouldEqual, 20)
+		})
+	})
+}
+
+func TestNewRecordPager(t *testing.T) {
+	convey.Convey("TestNewRecordPager", t, func() {
+		convey.Convey("NewRecordPager", func() {
+			pager := NewRecordPager(2, 10, 20, 1)
+			convey.So(pager.TotalPage, convey.ShouldEqual, 2)
+			convey.So(pager.GetTotal(), convey.ShouldEqual, 20)
 		})
 	})
 }
