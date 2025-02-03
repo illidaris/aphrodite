adaa3c38280b77a474884fa64ec6636490ced3c4
Joseph <ivan_whisper@outlook.com>
2025-02-03 19:49:54 +0800

    add level

--- component/gormex/transaction.go
+++ component/gormex/transaction.go
@@ -2,6 +2,7 @@ package gormex
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 
 	"github.com/illidaris/aphrodite/pkg/contextex"
@@ -12,14 +13,41 @@ import (
 
 var _ = dependency.IUnitOfWork(&GormTransactionImpl{})
 
+type UOWOptions struct {
+	IsolationLevel sql.IsolationLevel
+}
+
+type UOWOptionFunc func(*UOWOptions)
+
+func WithIsolationLevel(level sql.IsolationLevel) UOWOptionFunc {
+	return func(o *UOWOptions) {
+		o.IsolationLevel = level
+	}
+}
+
 func GetDbTX(id string) contextex.ContextKey {
 	return contextex.DbTxID.ID(id)
 }
-func NewUnitOfWork(id string) dependency.IUnitOfWork {
-	return &GormTransactionImpl{
+func NewUnitOfWork(id string, opts ...UOWOptionFunc) dependency.IUnitOfWork {
+	return NewGormUnitOfWork(id, opts...)
+}
+
+func NewGormUnitOfWork(id string, opts ...UOWOptionFunc) *GormTransactionImpl {
+	opt := &UOWOptions{}
+	for _, o := range opts {
+		o(opt)
+	}
+	impl := &GormTransactionImpl{
 		id: id,
 		db: GetTransactionDb(id),
 	}
+	// 自定义事务隔离等级
+	if opt.IsolationLevel != sql.LevelDefault {
+		impl.txOpts = &sql.TxOptions{
+			Isolation: opt.IsolationLevel,
+		}
+	}
+	return impl
 }
 
 func GetTransactionDb(id string) *gorm.DB {
@@ -27,13 +55,24 @@ func GetTransactionDb(id string) *gorm.DB {
 }
 
 type GormTransactionImpl struct {
-	id string
-	db *gorm.DB
+	id     string
+	db     *gorm.DB
+	txOpts *sql.TxOptions
+}
+
+func (t *GormTransactionImpl) WithTxOpts(txOpts *sql.TxOptions) dependency.IUnitOfWork {
+	t.txOpts = txOpts
+	return t
 }
 
 // Execute An execution function is passed in, and transactions are executed within the function.
 func (t *GormTransactionImpl) Execute(ctx context.Context, fs ...dependency.DbAction) (e error) {
-	tx := t.db.WithContext(ctx).Begin()
+	var tx *gorm.DB
+	if t.txOpts != nil {
+		tx = t.db.WithContext(ctx).Begin(t.txOpts)
+	} else {
+		tx = t.db.WithContext(ctx).Begin()
+	}
 	tx.Logger.Info(ctx, "transaction begin")
 	defer func() {
 		if r := recover(); r != nil {
