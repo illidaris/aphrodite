ff8a2e1217e9ee19dc3d9aa40e3e89f9181f582a
Joseph <ivan_whisper@outlook.com>
2025-06-25 10:47:47 +0800

    fix

--- biz/api/api.go
+++ biz/api/api.go
@@ -9,52 +9,60 @@ import (
 	"github.com/illidaris/rest/signature"
 )
 
-func POST[In IRequest, Out any](ctx context.Context, req In, host, secret string) (*Out, error) {
-	r := NewPostAPI[In, Out](req)
-	err := invoke(ctx, r, host, secret, time.Second*5)
-	if err != nil {
-		return nil, err
+func POST[In IRequest, Out any](host, secret string, timeout time.Duration) func(ctx context.Context, req In) (*Out, error) {
+	return func(ctx context.Context, req In) (*Out, error) {
+		r := NewPostAPI[In, Out](req)
+		err := invoke(ctx, r, host, secret, timeout)
+		if err != nil {
+			return nil, err
+		}
+		if r.Response == nil {
+			return nil, fmt.Errorf("[POST]%v resp is nil", req.GetPath())
+		}
+		return r.Response.Data, nil
 	}
-	if r.Response == nil {
-		return nil, fmt.Errorf("[POST]%v resp is nil", req.GetPath())
-	}
-	return r.Response.Data, nil
 }
 
-func PUT[In IRequest, Out any](ctx context.Context, req In, host, secret string) (*Out, error) {
-	r := NewPutAPI[In, Out](req)
-	err := invoke(ctx, r, host, secret, time.Second*5)
-	if err != nil {
-		return nil, err
+func PUT[In IRequest, Out any](host, secret string, timeout time.Duration) func(ctx context.Context, req In) (*Out, error) {
+	return func(ctx context.Context, req In) (*Out, error) {
+		r := NewPutAPI[In, Out](req)
+		err := invoke(ctx, r, host, secret, time.Second*5)
+		if err != nil {
+			return nil, err
+		}
+		if r.Response == nil {
+			return nil, fmt.Errorf("[PUT]%v resp is nil", req.GetPath())
+		}
+		return r.Response.Data, nil
 	}
-	if r.Response == nil {
-		return nil, fmt.Errorf("[PUT]%v resp is nil", req.GetPath())
-	}
-	return r.Response.Data, nil
 }
 
-func GET[In IRequest, Out any](ctx context.Context, req In, host, secret string) (*Out, error) {
-	r := NewGetAPI[In, Out](req)
-	err := invoke(ctx, r, host, secret, time.Second*5)
-	if err != nil {
-		return nil, err
+func GET[In IRequest, Out any](host, secret string, timeout time.Duration) func(ctx context.Context, req In) (*Out, error) {
+	return func(ctx context.Context, req In) (*Out, error) {
+		r := NewGetAPI[In, Out](req)
+		err := invoke(ctx, r, host, secret, time.Second*5)
+		if err != nil {
+			return nil, err
+		}
+		if r.Response == nil {
+			return nil, fmt.Errorf("[GET]%v resp is nil", req.GetPath())
+		}
+		return r.Response.Data, nil
 	}
-	if r.Response == nil {
-		return nil, fmt.Errorf("[GET]%v resp is nil", req.GetPath())
-	}
-	return r.Response.Data, nil
 }
 
-func DELETE[In IRequest, Out any](ctx context.Context, req In, host, secret string) (*Out, error) {
-	r := NewDeleteAPI[In, Out](req)
-	err := invoke(ctx, r, host, secret, time.Second*5)
-	if err != nil {
-		return nil, err
+func DELETE[In IRequest, Out any](host, secret string, timeout time.Duration) func(ctx context.Context, req In) (*Out, error) {
+	return func(ctx context.Context, req In) (*Out, error) {
+		r := NewDeleteAPI[In, Out](req)
+		err := invoke(ctx, r, host, secret, time.Second*5)
+		if err != nil {
+			return nil, err
+		}
+		if r.Response == nil {
+			return nil, fmt.Errorf("[DELETE]%v resp is nil", req.GetPath())
+		}
+		return r.Response.Data, nil
 	}
-	if r.Response == nil {
-		return nil, fmt.Errorf("[DELETE]%v resp is nil", req.GetPath())
-	}
-	return r.Response.Data, nil
 }
 
 func invoke(ctx context.Context, req restSender.IRequest, host, secret string, timeout time.Duration) error {
