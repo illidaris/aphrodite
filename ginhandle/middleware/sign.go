006331cbc838f56d02c910089e935521e5c5b1a7
Joseph Wang <ivan_whisper@outlook.com>
2026-01-08 16:27:41 +0800

    fix

--- ginhandle/middleware/sign.go
+++ ginhandle/middleware/sign.go
@@ -24,18 +24,19 @@ func WebSignMiddleware(sopts ...WebsignOption) gin.HandlerFunc {
 		tsStr := c.Query(signature.SignKeyTimestamp)
 		// 校验签名
 		if !opts.Skip(ctx) {
-			secret, ok := opts.Secrets[app]
-			if !ok {
+			secret := opts.SecretFunc(ctx, app)
+			if secret == "" {
 				c.AbortWithStatusJSON(http.StatusOK, dto.NewResponse(nil, exception.ERR_COMMON_SIGN_APP.New("应用访问被拒绝")))
 				return
 			}
-			if !opts.AllowVer(ver) {
+			if !opts.AllowVerFunc(ctx, ver) {
 				c.AbortWithStatusJSON(http.StatusOK, dto.NewResponse(nil, exception.ERR_COMMON_SIGN_VER.New("签名版本失效")))
 				return
 			}
+			timeout := opts.TimeoutFunc(ctx)
 			now := time.Now()
-			beg := now.Add(-1 * opts.Timeout)
-			end := now.Add(opts.Timeout)
+			beg := now.Add(-1 * timeout)
+			end := now.Add(timeout)
 			if ts := cast.ToInt64(tsStr); beg.Unix() > ts || end.Unix() < ts {
 				c.AbortWithStatusJSON(http.StatusOK, dto.NewResponse(nil, exception.ERR_COMMON_SIGN_EXPIRED.New("签名已过期")))
 				return
@@ -44,7 +45,7 @@ func WebSignMiddleware(sopts ...WebsignOption) gin.HandlerFunc {
 				signature.WithHmacFunc(func(s string, as ...string) string {
 					return signature.HashMac(sha256.New, s, as...)
 				}),
-				signature.WithExpire(opts.Timeout),
+				signature.WithExpire(timeout),
 				signature.WithSecret(secret),
 			}
 			realOpts = append(realOpts, opts.RestOptions...)
@@ -67,11 +68,11 @@ type WebsignOption func(*WebsignOptions)
 // NewWebsignOptions 创建并返回一个新的WebsignOptions实例，初始化允许的主机和版本为空，超时时间为2分钟，签名不可用。
 func NewWebsignOptions(opts ...WebsignOption) *WebsignOptions {
 	o := &WebsignOptions{
-		AllowHosts:  map[string]struct{}{},
-		AllowVers:   map[string]struct{}{},
-		Timeout:     time.Minute * 3,
-		Secrets:     map[string]string{},
-		RestOptions: []signature.OptionFunc{},
+		AllowHostFunc: func(ctx context.Context, s string) bool { return true },
+		AllowVerFunc:  func(ctx context.Context, s string) bool { return true },
+		TimeoutFunc:   func(ctx context.Context) time.Duration { return time.Minute * 3 },
+		SecretFunc:    func(ctx context.Context, s string) string { return "" },
+		RestOptions:   []signature.OptionFunc{},
 	}
 	for _, opt := range opts {
 		opt(o)
@@ -85,39 +86,27 @@ func WithSkipFunc(v func(context.Context) bool) WebsignOption {
 	}
 }
 
-func WithSecret(app, secret string) WebsignOption {
+func WithSecretFunc(v func(ctx context.Context, s string) string) WebsignOption {
 	return func(opts *WebsignOptions) {
-		opts.Secrets[app] = secret
+		opts.SecretFunc = v
 	}
 }
 
-func WithSecrets(secrets map[string]string) WebsignOption {
+func WithExpireFunc(v func(ctx context.Context) time.Duration) WebsignOption {
 	return func(opts *WebsignOptions) {
-		for k, v := range secrets {
-			opts.Secrets[k] = v
-		}
+		opts.TimeoutFunc = v
 	}
 }
 
-func WithExpire(timeout time.Duration) WebsignOption {
+func WithHostFunc(v func(ctx context.Context, s string) bool) WebsignOption {
 	return func(opts *WebsignOptions) {
-		opts.Timeout = timeout
+		opts.AllowHostFunc = v
 	}
 }
 
-func WithHosts(hosts ...string) WebsignOption {
+func WithVerFunc(v func(ctx context.Context, s string) bool) WebsignOption {
 	return func(opts *WebsignOptions) {
-		for _, v := range hosts {
-			opts.AllowHosts[v] = struct{}{}
-		}
-	}
-}
-
-func WithVers(vers ...string) WebsignOption {
-	return func(opts *WebsignOptions) {
-		for _, v := range vers {
-			opts.AllowVers[v] = struct{}{}
-		}
+		opts.AllowVerFunc = v
 	}
 }
 
@@ -130,12 +119,12 @@ func WithRestOptions(vs ...signature.OptionFunc) WebsignOption {
 // WebsignOptions 定义了Web签名选项的结构体。
 // 其中包括是否启用签名、签名密钥、超时时间、允许的主机和允许的版本。
 type WebsignOptions struct {
-	Secrets     map[string]string      // 签名所用的密钥
-	Timeout     time.Duration          // 请求超时时间
-	AllowHosts  map[string]struct{}    // 允许签名的主机名集合
-	AllowVers   map[string]struct{}    // 允许签名的版本集合
-	RestOptions []signature.OptionFunc // 框架参数
-	SkipFunc    func(context.Context) bool
+	SecretFunc    func(context.Context, string) string // 签名所用的密钥
+	TimeoutFunc   func(context.Context) time.Duration  // 请求超时时间
+	AllowHostFunc func(context.Context, string) bool   // 允许签名的主机名集合
+	AllowVerFunc  func(context.Context, string) bool   // 允许签名的版本集合
+	RestOptions   []signature.OptionFunc               // 框架参数
+	SkipFunc      func(context.Context) bool
 }
 
 func (opts *WebsignOptions) Skip(ctx context.Context) bool {
@@ -143,37 +132,4 @@ func (opts *WebsignOptions) Skip(ctx context.Context) bool {
 		return false
 	}
 	return opts.SkipFunc(ctx)
-}
-
-// AllowHost 判断给定的主机名是否在允许的主机名集合中。
-// v: 要判断的主机名
-// 返回值表示是否允许该主机名进行签名。
-func (opts *WebsignOptions) AllowHost(v string) bool {
-	if opts.AllowHosts == nil {
-		return false
-	}
-	for k := range opts.AllowHosts {
-		if k == "*" {
-			return true
-		}
-	}
-	if len(v) == 0 {
-		return false
-	}
-	_, ok := opts.AllowHosts[v]
-	return ok
-}
-
-// AllowVer 判断给定的版本是否在允许的版本集合中。
-// v: 要判断的版本号
-// 返回值表示是否允许该版本进行签名。
-func (opts *WebsignOptions) AllowVer(v string) bool {
-	if len(v) == 0 {
-		return false
-	}
-	if opts.AllowVers == nil {
-		return false
-	}
-	_, ok := opts.AllowVers[v]
-	return ok
 }
