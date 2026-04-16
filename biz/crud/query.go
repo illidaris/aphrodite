fbac4da8d4f60af49ae640c38d080d0203be4668
Joseph Wang <ivan_whisper@outlook.com>
2026-04-16 16:25:07 +0800

    add

--- biz/crud/query.go
+++ biz/crud/query.go
@@ -58,6 +58,43 @@ func PagingListFunc[T dependency.IEntity, K dto.IRow](repo dependency.IRepositor
 	}
 }
 
+func PageListFunc[T dependency.IEntity](repo dependency.IRepository[T], opts ...Option) func(ctx context.Context, req dependency.ICondPage) (*dto.RecordPtrPager[T], exception.Exception) {
+	return func(ctx context.Context, req dependency.ICondPage) (*dto.RecordPtrPager[T], exception.Exception) {
+		option := &Options{
+			RepoOptions: []dependency.BaseOptionFunc{
+				dependency.WithPage(req),
+				dependency.WithReadOnly(true),
+			},
+		}
+
+		option.RepoOptions = append(option.RepoOptions, dependency.ShardingOptions(req)...)
+		if keys := req.GetConds(); len(keys) > 0 {
+			option.RepoOptions = append(option.RepoOptions, dependency.WithConds(keys...))
+		}
+
+		for _, opt := range opts {
+			opt(option)
+		}
+
+		result := new(dto.RecordPtrPager[T])
+		result.PageIndex = req.GetPageIndex()
+		result.PageSize = req.GetPageSize()
+		ps, total, err := repo.BaseQueryWithCount(ctx, option.RepoOptions...)
+		if err != nil {
+			return result, exception.ERR_BUSI.Wrap(err)
+		}
+		data := make([]*T, 0)
+		for _, v := range ps {
+			vv := v
+			data = append(data, &vv)
+		}
+		result.Data = data
+		result.TotalRecord = total
+		result.Paginator()
+		return result, nil
+	}
+}
+
 func CountFunc[T dependency.IEntity](repo dependency.IRepository[T], opts ...Option) func(ctx context.Context, req dependency.ICond) (int64, exception.Exception) {
 	return func(ctx context.Context, req dependency.ICond) (int64, exception.Exception) {
 		option := &Options{
--- ginhandle/crud.go
+++ ginhandle/crud.go
@@ -12,50 +12,50 @@ import (
 	"github.com/jinzhu/copier"
 )
 
-func ListHandler[Req dependency.ICondPage, T dependency.IEntity]() func(c *gin.Context) {
+func ListHandler[Req dependency.ICondPage, T dependency.IEntity](opts ...crud.Option) func(c *gin.Context) {
 	return GinOneHandler(func(ctx context.Context, r *Req) (*dto.RecordPtrPager[T], exception.Exception) {
 		repo := &gormex.BaseRepository[T]{}
-		return crud.PagingListFunc[T, T](repo, nil)(ctx, *r)
+		return crud.PageListFunc(repo, opts...)(ctx, *r)
 	})
 }
 
-func CreateManyHandler[Req any, T dependency.IEntity](f func(Req) []*T) func(c *gin.Context) {
+func CreateManyHandler[Req any, T dependency.IEntity](f func(Req) []*T, opts ...crud.Option) func(c *gin.Context) {
 	return GinOneHandler(func(ctx context.Context, r *Req) (int64, exception.Exception) {
 		repo := &gormex.BaseRepository[T]{}
-		return crud.Create(repo, nil)(ctx, f(*r))
+		return crud.Create(repo, nil, opts...)(ctx, f(*r))
 	})
 }
 
-func CreateHandler[Req any, T dependency.IEntity]() func(c *gin.Context) {
-	return GinOneHandler(func(ctx context.Context, r *Req) (int64, exception.Exception) {
-		repo := &gormex.BaseRepository[T]{}
-		t := new(T)
-		_ = copier.Copy(t, r)
-		return crud.Create(repo, nil)(ctx, []*T{t})
-	})
-}
-
-func UpdateHandler[Req dependency.ICond, T dependency.IEntity]() func(c *gin.Context) {
+func CreateHandler[Req any, T dependency.IEntity](opts ...crud.Option) func(c *gin.Context) {
 	return GinOneHandler(func(ctx context.Context, r *Req) (int64, exception.Exception) {
 		repo := &gormex.BaseRepository[T]{}
 		t := new(T)
 		_ = copier.Copy(t, r)
-		return crud.Update(repo, nil)(ctx, t, (*r).GetConds()...)
+		return crud.Create(repo, nil, opts...)(ctx, []*T{t})
 	})
 }
 
-func DeleteHandler[Req dependency.ICond, T dependency.IEntity]() func(c *gin.Context) {
+func UpdateHandler[Req dependency.ICond, T dependency.IEntity](opts ...crud.Option) func(c *gin.Context) {
 	return GinOneHandler(func(ctx context.Context, r *Req) (int64, exception.Exception) {
 		repo := &gormex.BaseRepository[T]{}
 		t := new(T)
 		_ = copier.Copy(t, r)
-		return crud.Delete(repo, nil)(ctx, r, (*r).GetConds()...)
+		return crud.Update(repo, nil, opts...)(ctx, t, (*r).GetConds()...)
 	})
 }
 
-func DetailHandler[Req dependency.ICond, T dependency.IEntity]() func(c *gin.Context) {
+func DeleteHandler[Req dependency.ICond, T dependency.IEntity](opts ...crud.Option) func(c *gin.Context) {
+	return GinOneHandler(func(ctx context.Context, r *Req) (int64, exception.Exception) {
+		repo := &gormex.BaseRepository[T]{}
+		t := new(T)
+		_ = copier.Copy(t, r)
+		return crud.Delete(repo, opts...)(ctx, r, (*r).GetConds()...)
+	})
+}
+
+func DetailHandler[Req dependency.ICond, T dependency.IEntity](opts ...crud.Option) func(c *gin.Context) {
 	return GinOneHandler(func(ctx context.Context, r *Req) (*T, exception.Exception) {
 		repo := &gormex.BaseRepository[T]{}
-		return crud.DetailFunc(repo, nil)(ctx, *r)
+		return crud.DetailFunc(repo, opts...)(ctx, *r)
 	})
 }
