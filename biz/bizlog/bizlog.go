6064659d1a1b630b50ab51ab6591980611c16383
Joseph Wang <ivan_whisper@outlook.com>
2026-05-07 18:53:09 +0800

    add

---
+++ biz/bizlog/bizlog.go
@@ -0,0 +1,92 @@
+package bizlog
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/illidaris/core"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var once sync.Once
+var l *Logger
+
+type Entry struct {
+	Uid      int64    `json:"ibzl_uid,omitempty"`      // 【WHO】主角Id
+	Pid      string   `josn:"ibzl_pid,omitempty"`      // 【WHO】主角PID
+	Name     string   `json:"ibzl_name,omitempty"`     // 【WHO】主角名
+	BizId    int64    `json:"ibzl_bizid,omitempty"`    // 【WHERE】业务Id
+	System   string   `json:"ibzl_system,omitempty"`   // 【WHERE】所属系统
+	Message  string   `json:"ibzl_msg,omitempty"`      // 【WHAT】什么事
+	Tags     []string `json:"ibzl_tags,omitempty"`     // 标签
+	OpAt     int64    `json:"ibzl_opat,omitempty"`     // 【WHEN】发生时间点
+	CreateAt int64    `json:"ibzl_createat,omitempty"` // 入库时间点
+	Dst      int32    `json:"log_dst,omitempty"`       // 日志用途 兼容之前的设计
+	TraceId  string   `json:"traceId,omitempty"`       // 追踪ID 兼容之前的设计
+}
+
+type Logger struct {
+	Opts []Option
+	log  *zap.Logger
+}
+
+func NewInstance(opts ...Option) *Logger {
+	once.Do(func() {
+		l = &Logger{
+			Opts: opts,
+			log:  zap.L().WithOptions(zap.AddCallerSkip(1)),
+		}
+	})
+	return l
+}
+
+func (l *Logger) DebugFunc(opts ...Option) func(ctx context.Context, msg string, args ...any) {
+	return func(ctx context.Context, msg string, args ...any) {
+		l.Log(ctx, opts, l.log.Debug, msg, args...)
+	}
+}
+
+func (l *Logger) InfoFunc(opts ...Option) func(ctx context.Context, msg string, args ...any) {
+	return func(ctx context.Context, msg string, args ...any) {
+		l.Log(ctx, opts, l.log.Debug, msg, args...)
+	}
+}
+
+func (l *Logger) WarnFunc(opts ...Option) func(ctx context.Context, msg string, args ...any) {
+	return func(ctx context.Context, msg string, args ...any) {
+		l.Log(ctx, opts, l.log.Debug, msg, args...)
+	}
+}
+
+func (l *Logger) ErrorFunc(opts ...Option) func(ctx context.Context, msg string, args ...any) {
+	return func(ctx context.Context, msg string, args ...any) {
+		l.Log(ctx, opts, l.log.Debug, msg, args...)
+	}
+}
+
+func (l *Logger) Log(ctx context.Context,
+	opts []Option,
+	f func(msg string, fields ...zapcore.Field),
+	msg string, args ...any) {
+
+	fields := WithTrace(ctx)
+
+	fields = append(fields,
+		NewOptions().
+			WithOption(l.Opts...).
+			WithOption(opts...).
+			Fields()...)
+
+	f(fmt.Sprintf(msg, args...), fields...)
+}
+
+func WithTrace(ctx context.Context) []zapcore.Field {
+	traceID := core.TraceID.GetString(ctx)
+	sessionID := core.SessionID.GetString(ctx)
+	return []zapcore.Field{
+		zap.String(core.TraceID.String(), traceID),
+		zap.String(core.SessionID.String(), sessionID),
+	}
+}
---
+++ biz/bizlog/option.go
@@ -0,0 +1,112 @@
+package bizlog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// OptionFunc 用于配置 Options 的函数选项类型
+type Option func(*Options)
+
+func NewOptions() *Options {
+	return &Options{
+		Entry: Entry{
+			Tags: []string{},
+		},
+	}
+}
+
+// Options 业务日志可选配置
+type Options struct {
+	Entry
+}
+
+func (o *Options) WithOption(opts ...Option) *Options {
+	for _, opt := range opts {
+		opt(o)
+	}
+	return o
+}
+
+func (o *Options) Fields() []zapcore.Field {
+	return []zapcore.Field{
+		zap.Int32("log_dst", o.Dst),
+		zap.Int64("ibzl_uid", o.Uid),
+		zap.String("ibzl_pid", o.Pid),
+		zap.String("ibzl_name", o.Name),
+		zap.Int64("ibzl_bizid", o.BizId),
+		zap.String("ibzl_system", o.System),
+		zap.Strings("ibzl_tags", o.Tags),
+		zap.Int64("ibzl_opat", o.OpAt),
+	}
+}
+
+// WithDst 日志分类
+func WithDst(v int32) Option {
+	return func(o *Options) {
+		o.Dst = v
+	}
+}
+
+// WithUid 设置主角 Id（WHO）
+func WithUid(uid int64) Option {
+	return func(o *Options) {
+		o.Uid = uid
+	}
+}
+
+// WithPid 设置主角 PID（WHO）
+func WithPid(pid string) Option {
+	return func(o *Options) {
+		o.Pid = pid
+	}
+}
+
+// WithName 设置主角名（WHO）
+func WithName(name string) Option {
+	return func(o *Options) {
+		o.Name = name
+	}
+}
+
+// WithBizId 设置业务 Id（WHERE）
+func WithBizId(bizId int64) Option {
+	return func(o *Options) {
+		o.BizId = bizId
+	}
+}
+
+// WithSystem 设置所属系统（WHERE）
+func WithSystem(system string) Option {
+	return func(o *Options) {
+		o.System = system
+	}
+}
+
+// WithMessage 设置事件描述（WHAT）
+func WithMessage(message string) Option {
+	return func(o *Options) {
+		o.Message = message
+	}
+}
+
+// WithTags 追加标签
+func WithTags(tags ...string) Option {
+	return func(o *Options) {
+		o.Tags = append(o.Tags, tags...)
+	}
+}
+
+// WithOpAt 设置事件发生时间点（WHEN）
+func WithOpAt(opAt int64) Option {
+	return func(o *Options) {
+		o.OpAt = opAt
+	}
+}
+
+// WithCreateAt 设置入库时间点
+func WithCreateAt(createAt int64) Option {
+	return func(o *Options) {
+		o.CreateAt = createAt
+	}
+}
