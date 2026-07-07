d43a38f309465c10bbe9ffd880debea4d2580ee9
Joseph Wang <ivan_whisper@outlook.com>
2026-07-07 18:17:40 +0800

    add

--- pkg/convert/string.go
+++ pkg/convert/string.go
@@ -2,7 +2,6 @@ package convert
 
 import (
 	"encoding/json"
-	"unicode/utf8"
 )
 
 // Json json marshal
@@ -20,15 +19,9 @@ func TruncateMySQLVarchar(raw string, length int) string {
 	if length <= 0 || raw == "" {
 		return ""
 	}
-
-	used := 0
-	for i, r := range raw {
-		size := utf8.RuneLen(r)
-		if used+size > length {
-			return raw[:i]
-		}
-		used += size
+	runes := []rune(raw)
+	if len(runes) <= length {
+		return raw
 	}
-
-	return raw
+	return string(runes[:length])
 }
